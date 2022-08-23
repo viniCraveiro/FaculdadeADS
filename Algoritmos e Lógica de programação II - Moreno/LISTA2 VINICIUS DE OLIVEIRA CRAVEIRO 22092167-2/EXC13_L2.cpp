8556e31097674ab2445bbdc620be3307f58cd440
Vinicius Craveiro <99699904+vnCraveiro@users.noreply.github.com>
2022-08-23 20:34:21 -0300

    EXC13_L2 Feito

--- Algoritmos e Lógica de programação II - Moreno/LISTA2 VINICIUS DE OLIVEIRA CRAVEIRO 22092167-2/EXC13_L2.cpp
+++ Algoritmos e Lógica de programação II - Moreno/LISTA2 VINICIUS DE OLIVEIRA CRAVEIRO 22092167-2/EXC13_L2.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 //13) Solicite dois nomes e apresente qual nome é maior (segundo a ordem alfabética)
 int main() {
 	char nome1[50], nome2[50];
-	int i=1;
+	int i=1, retorno;
 	
 	printf("Informe o %d nome: ",i++);
 	gets(nome1);
@@ -12,13 +14,20 @@ int main() {
 	printf("Informe o %d nome: ",i++);
 	gets(nome2);
 	
-	if(nome1<nome2){
-		printf("Nome Maior: %s\n"
-		"Nome Menor: %s\n\n",nome1,nome2);
-	}else{
-		printf("Nome Maior: %s\n"
-		"Nome Menor: %s\n\n",nome2,nome1);
-	}
+	//strcpy (nome1,toupper(nome1));
+	//nome2 = toupper(nome2);
+	
+  retorno = strcmp(nome1,nome2);
+  printf("\n\nretorno= %d", retorno);
+  if (retorno == -1)
+     printf("\nNome Menor(A-Z): %s"
+	 	"\nNome Maior(Z-A): %s\n",&nome1,&nome2);
+  else
+     if (retorno == 1)
+       printf("\nNome Menor(A-Z): %s"
+	 	"\nNome Maior(Z-A): %s\n",&nome2,&nome1);
+      else
+        printf("\nOs nomes sao iguais");
 
 		
 
