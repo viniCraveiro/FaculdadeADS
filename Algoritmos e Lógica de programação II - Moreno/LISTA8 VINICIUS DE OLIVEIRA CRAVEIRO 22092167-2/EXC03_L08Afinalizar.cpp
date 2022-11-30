ac9e10b6ea2a57c400df606d173e8881bb896267
Vinicius Craveiro <viniciuscraveiroo@gmail.com>
2022-11-29 23:08:55 -0300

    Introducao ao Bootstrap

--- Algoritmos e Lógica de programação II - Moreno/LISTA8 VINICIUS DE OLIVEIRA CRAVEIRO 22092167-2/EXC03_L08Afinalizar.cpp
+++ Algoritmos e Lógica de programação II - Moreno/LISTA8 VINICIUS DE OLIVEIRA CRAVEIRO 22092167-2/EXC03_L08Afinalizar.cpp
@@ -1,11 +1,12 @@
-//2) Solicite 10 nomes e armazene em um vetor denominado V1. 
-//Posteriormente, classifique-o em ordem crescente e apresente ao usuário. Utilize o selection sort
+//3) Solicite 6 nomes e armazene em um vetor denominado(nome. 
+//Posteriormente, classifique o vetor V2 (de mesmo tamanho e tipo) 
+//contendo os dados de(nome. Apresente V2 no final. Utilize o bubble sort.
 #include<stdio.h>
 #include<stdlib.h>
 #include <string.h>
 #define tot 3
 
-char v1[10][50];
+char nome[tot][50];
 void entradaDados();
 void classificaDados();
 void apresentaDados();
@@ -22,7 +23,8 @@ void entradaDados(){
 	int x;
 	for (x=0; x<tot; x++){
 		printf("Informe o nome: ");
-		gets(v1[x]);
+		scanf("%s",nome[x]);
+		fflush(stdin);
 	}
 	classificaDados();
 	
@@ -33,10 +35,10 @@ void classificaDados(){
 	char tmp[50];
 	for(x=0; x<tot-1; x++){
 		for (y=x+1;y<tot; y++){
-			if(strcmp(v1[x],v1[y])==1){
-				strcpy(tmp,v1[x]);
-				strcpy(v1[x],v1[y]);
-				strcpy(v1[y],tmp);
+			if(strcmp(nome[x],nome[y])==1){
+				strcpy(tmp,nome[x]);
+				strcpy(nome[x],nome[y]);
+				strcpy(nome[y],tmp);
 			}
 		}
 	}
@@ -45,7 +47,7 @@ void classificaDados(){
 void apresentaDados(){
 	int x,y=1;
 	for(x=0;x<tot;x++){
-		printf("Nome %d: %s\n",y,v1[x]);
+		printf("Nome %d: %s\n",y,nome[x]);
 		y++;
 	}
 }
